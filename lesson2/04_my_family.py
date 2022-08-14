5246d0b0c073e9612419dcb3285d9efc72d6474f
illirel <ilya.kononov.09@gmai.com>
2022-08-14 14:39:04 +0500

    done

--- lesson2/04_my_family.py
+++ lesson2/04_my_family.py
@@ -12,11 +12,23 @@ my_family_height = [
     # ['имя', рост],
     ['lena', 168], ['Aleksey',188],['Diana', 165], ['Rimma',150],['Anton', 188]
 ]
-
+mfh = my_family_height
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 print('Рост отца-')
-print((my_family_height[1])[1])
+print(mfh[1][1])
 print('см')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+l=len(my_family_height)
+
+h = []
+for i in mfh:
+    for n in i:
+        if type(n)==int:
+            h.append(n)
+print(sum(h))
+
+print('Общий рост моей семьи-' + str(sum(h)) + ' см')
+
+
