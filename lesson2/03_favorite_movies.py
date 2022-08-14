0e9c384e33047b00516e2da287fb527bed3de682
illirel <ilya.kononov.09@gmai.com>
2022-08-14 10:42:16 +0500

    3

--- lesson2/03_favorite_movies.py
+++ lesson2/03_favorite_movies.py
@@ -20,8 +20,25 @@ l = len(q)
 n = q.find(', ')
 print(q[0:(n)])
 
-n = q.rfind(', ')
-print(q[n+2:l])
+rn = q.rfind(', ')
+print(q[rn+2:l])
+
+
+n = q.find(', ',start = q[n], end= ', ')
+
+
+TEMP = str()
+FilmList = list()
+
+for i in q:
+    if i != ', ':
+        TEMP += i
+    else:
+        FilmList.append(TEPM)
+        TEMP = str()
+    print(FilmList)
+
+
 
 
 
