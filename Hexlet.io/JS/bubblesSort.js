78983b7a125f608d81c75e603180de56d10f7ead
Ilya illirel <ilya.kononov.09@gmail.com>
2024-02-18 21:57:59 +0500

    ыы

--- Hexlet.io/JS/bubblesSort.js
+++ Hexlet.io/JS/bubblesSort.js
@@ -1,20 +1,20 @@
 const bubbleSort = arr => {
-	let stepsCount = arr.length - 1
+	let stepsCount = arr.length - 1;
 
-	let swapped
-
+	let swapped;
+	677;
 	do {
-		swapped = false
+		swapped = false;
 		for (let i = 0; i < stepsCount; i += 1) {
 			if (arr[i] > arr[i + 1]) {
-				const temp = arr[i]
-				arr[i] = arr[i + 1]
-				arr[i + 1] = temp
-				swapped = true
+				const temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
 			}
 		}
-		stepsCount -= 1
-	} while (swapped)
-	return arr
-}
-console.log(bubbleSort([3, 2, 10, -2, 0]))
+		stepsCount -= 1;
+	} while (swapped);
+	return arr;
+};
+console.log(bubbleSort([3, 2, 10, -2, 0]));
