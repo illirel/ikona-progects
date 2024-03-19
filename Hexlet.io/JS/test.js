9e556273ae07d90238c0054c3df41333f9d74a25
Ilya illirel <ilya.kononov.09@gmail.com>
2024-03-19 23:54:31 +0500

    objects test

--- Hexlet.io/JS/test.js
+++ Hexlet.io/JS/test.js
@@ -1,2 +1,26 @@
-console.log(3);
-hello;
+function get(data, keys) {
+  let result = data;
+  for (const key of keys) {
+    if (!Object.hasOwn(result, key)) {
+      return null;
+    }
+    result = result[key];
+  }
+  return result;
+}
+
+const data = {
+  user: 'ubuntu',
+  hosts: {
+    0: {
+      name: 'web1',
+    },
+    1: {
+      name: 'web2',
+      null: 3,
+      active: false,
+    },
+  },
+};
+
+console.log(get(data, ['hosts', 1, 'name']));
